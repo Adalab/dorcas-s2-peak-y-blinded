212046096c26509dbd7c327364a8cdff8cba6fd0
oljuanfe <ojuanferrero@gmail.com>
2018-07-07 11:01:54 +0200

    Create divs containing select,add css classes

--- js/select-buttons.js
+++ js/select-buttons.js
@@ -1,10 +1,10 @@
 'use strict';
 
 //Apuntar al padre de Select, el div
-var parentSelect = document.querySelector('.js__select-container');
+var parentDivSelect = document.querySelector('.js__select-container');
 
 //Declaracione de variables de select y su contenido
-
+var newParentSelect = document.createElement('div');
 var newSelect = document.createElement('select');
 var newOption = document.createElement('option');
 var optionContent = document.createTextNode('HTML');
@@ -18,11 +18,12 @@ console.log(newSelect);
 console.log(newOption);
 
 //Insercion contenido en option, e insercion de option como contenido de select. Insercion de select dentro del div que es su padre
-
 newOption.appendChild(optionContent);
 newSelect.appendChild(newOption);
-parentSelect.appendChild(newSelect);
+newParentSelect.appendChild(newSelect);
+parentDivSelect.appendChild(newParentSelect);
 
+newParentSelect.classList.add('item__select-container');
 newSelect.classList.add('item__select');
 
 //Insercion clase font-awesome en <i>, insercion <i> en <div>
@@ -30,5 +31,6 @@ newSelectButtonContent.classList.add('fas');
 newSelectButtonContent.classList.add('fa-plus');
 newSelectButton.appendChild(newSelectButtonContent);
 newSelectButton.classList.add('item__select-button');
-parentSelect.appendChild(newSelectButton);
+newParentSelect.appendChild(newSelectButton);
 console.log(newSelectButton);
+console.log(newParentSelect);
