5c17c003adfdf15496a4f878b6bb511eac8211a4
Esther Pato <estherpato@gmail.com>
2018-07-10 11:08:05 +0200

    Add some logic to reset button

--- js/inputs.js
+++ js/inputs.js
@@ -85,8 +85,15 @@ var resetButton = document.querySelector('.button-reset');
 
 function resetForm() {
   document.getElementById('form').reset();
-  linkID.classList.remove('contact__link--active');
-  linkID.classList.add('contact__link--inactive');
+  card.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
+  card.classList.add('paleta-azul');
+
+  cardInfo.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
+  cardInfo.classList.add('font-card--comicsans');
+
+  // var linkID = document.querySelector('.contact-link');
+  // linkID.classList.remove('contact__link--active');
+  // linkID.classList.add('contact__link--inactive');
 }
 
 resetButton.addEventListener('click', resetForm);
