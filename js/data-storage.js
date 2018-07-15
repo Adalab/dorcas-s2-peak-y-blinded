bf31936428616c1ba52e9c89202688e2df2450b8
oljuanfe <ojuanferrero@gmail.com>
2018-07-15 09:39:20 +0200

    Add favicon, get form data save in localStorage

--- js/data-storage.js
+++ js/data-storage.js
@@ -1,12 +1,12 @@
 'use strict';
 console.log('hola');
 //
-// var data = [];
-//
-// console.log(data);
-//
-// var inputStorage = document.querySelectorAll('.input__storage');
-//
+var data = {};
+
+console.log(data);
+
+var inputStorage = document.querySelectorAll('.input__storage');
+
 // function useDataStorage() {
 //   var dataInStorageName = localStorage.getItem(event.currentTarget.name);
 //   var dataInStorageValue = localStorage.getItem(event.currentTarget.value);
@@ -15,19 +15,26 @@ console.log('hola');
 //   event.currentTarget.value = data.dataInStorageValue;
 //   console.log('use data', event.currentTarget.value);
 // }
-//
-// function saveDataStorage(event) {
-//   var currentInput = event.currentTarget;
-//   console.log('current value', currentInput.value);
-//   console.log('current name', currentInput.name);
-//
-//   data[currentInput.name] = currentInput.value;
-//   console.log(data);
-//   localStorage.setItem(currentInput.name,currentInput.value);
-// }
-//
-//
-// for (var i = 0; i < inputStorage.length; i++ ){
-//   inputStorage[i].addEventListener('click',useDataStorage);
-//   inputStorage[i].addEventListener('change',saveDataStorage);
-// }
+
+function saveDataStorage(event) {
+  //Variable para actual nombre del imput y valor del input
+  var currentInput = event.currentTarget;
+  console.log('current value', currentInput.value);
+  console.log('current name', currentInput.name);
+  var currentInputName = currentInput.name;
+  var currentInputValue = currentInput.value;
+
+  //Guardo los datos dentro del objeto 'data'
+  data[currentInputName] = currentInputValue;
+  console.log('array data', data);
+
+  //Guardo datos individualmente y tambien como objeto 'data' que convierto en cadena
+  localStorage.setItem(currentInputName,currentInputValue);
+  localStorage.setItem('formData',JSON.stringify(data));
+}
+
+
+for (var i = 0; i < inputStorage.length; i++ ){
+  // inputStorage[i].addEventListener('click',useDataStorage);
+  inputStorage[i].addEventListener('change',saveDataStorage);
+}
