bcdb487a9d44bc76c96f9fd4070d22aa640cffd3
irenemherrero <irenemherrero@gmail.com>
2018-07-23 12:09:39 +0200

    solve problem data-storage

--- js/data-storage.js
+++ js/data-storage.js
@@ -3,14 +3,53 @@
 let data = {};
 let dataStorageRetrieved = [];
 
+
 // var fr = new FileReader();
 
 const inputStorage = document.querySelectorAll('.input__storage');
 
+//Recupero los datos de localStorage y los guardo en una variable
+
+const restoreDataForPreview = (dataStorageRetrieved) => {
+
+  document.querySelector('#element-name').innerText = dataStorageRetrieved.name;
+  document.querySelector('#element-role').innerText = dataStorageRetrieved.job;
+  document.querySelector('#element-mail').href = 'mailto:' + dataStorageRetrieved.email;
+  document.querySelector('#element-tel').href = 'tel:' + dataStorageRetrieved.phone;
+  document.querySelector('#element-lin').href = 'https://linkedin.com/in/' + dataStorageRetrieved.linkedin;
+  document.querySelector('#element-gh').href = 'https://github.com/' + dataStorageRetrieved.github;
+  document.querySelector('.personal-image').src = dataStorageRetrieved.photo;
+
+  const cardPreview = document.querySelector('#card');
+
+console.log(dataStorageRetrieved.palette);
+  if (dataStorageRetrieved.palette === '1') {
+    cardPreview.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
+    cardPreview.classList.add('paleta-azul');
+  } else if (dataStorageRetrieved.palette === '2') {
+    cardPreview.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
+    cardPreview.classList.add('paleta-roja');
+  } else if (dataStorageRetrieved.palette === '3') {
+    cardPreview.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
+    cardPreview.classList.add('paleta-gris');
+  }
+
+  if (dataStorageRetrieved.typography === '1') {
+    cardPreview.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
+    cardPreview.classList.add('font-card--ubuntu');
+  } else if (dataStorageRetrieved.typography === '2') {
+    cardPreview.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
+    cardPreview.classList.add('font-card--comicsans');
+  } else if (dataStorageRetrieved.typography === '3') {
+    cardPreview.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
+    cardPreview.classList.add('font-card--montserrat');
+  }
+}
 //Función para recuperar datos almacenados en localStorage
 const useDataStorage = (inputStorageName, inputStorageValue, inputStoragePosition) => {
   console.log('recuperando localStorage');
   dataStorageRetrieved = JSON.parse(localStorage.getItem('jsonToSend'));
+  console.dir(dataStorageRetrieved);
   restoreDataForPreview(dataStorageRetrieved);
 
   //Miro que el localStorage no esté vacio
@@ -61,51 +100,22 @@ const saveDataStorage = (event) => {
   localStorage.setItem('formData',JSON.stringify(data));
 }
 
-for (const dataStorage of inputStorage){
-  const inputStoragePosition = dataStorage;
-  const inputStorageName = dataStorage.name;
-  const inputStorageValue = dataStorage.value;
+//Bucle para recorrer los elementos input del formulario, es decir, los radiobuttons, inputs del formulario y skills. Todos esos elementos tienen la clase .input___storage.
+//De los elementos cojo su índice (dataStorage), su nombre (dataStorage.name) y su valor (dataStorage.value).
+//Una vez recorrido un elemento, hago una función que recupera los datos del local storage si los hay (useDataStorage).
+for (const data of inputStorage){
+  const inputStoragePosition = data;
+  const inputStorageName = data.name;
+  const inputStorageValue = data.value;
   useDataStorage(inputStorageName, inputStorageValue, inputStoragePosition);
-  dataStorage.addEventListener('change',saveDataStorage);
+  data.addEventListener('change',saveDataStorage);
 }
 
-for (const dataStorage of inputStorage){
-  dataStorage.addEventListener('change',saveDataStorage);
-}
 
-//Recupero los datos de localStorage y los guardo en una variable
+// for (const dataStorage of inputStorage){
+//   dataStorage.addEventListener('change',saveDataStorage);
+// }
 
-const restoreDataForPreview = (dataStorageRetrieved) => {
 
-  document.querySelector('#element-name').innerText = dataStorageRetrieved.name;
-  document.querySelector('#element-role').innerText = dataStorageRetrieved.job;
-  document.querySelector('#element-mail').href = 'mailto:' + dataStorageRetrieved.email;
-  document.querySelector('#element-tel').href = 'tel:' + dataStorageRetrieved.phone;
-  document.querySelector('#element-lin').href = 'https://linkedin.com/in/' + dataStorageRetrieved.linkedin;
-  document.querySelector('#element-gh').href = 'https://github.com/' + dataStorageRetrieved.github;
-  document.querySelector('.personal-image').src = dataStorageRetrieved.photo;
 
-  const cardPreview = document.querySelector('#card');
-console.log(dataStorageRetrieved.palette);
-  if (dataStorageRetrieved.palette === '1') {
-    cardPreview.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
-    cardPreview.classList.add('paleta-azul');
-  } else if (dataStorageRetrieved.palette === '2') {
-    cardPreview.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
-    cardPreview.classList.add('paleta-roja');
-  } else if (dataStorageRetrieved.palette === '3') {
-    cardPreview.classList.remove('paleta-azul', 'paleta-roja', 'paleta-gris');
-    cardPreview.classList.add('paleta-gris');
-  }
 
-  if (dataStorageRetrieved.typography === '1') {
-    cardPreview.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
-    cardPreview.classList.add('font-card--ubuntu');
-  } else if (dataStorageRetrieved.typography === '2') {
-    cardPreview.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
-    cardPreview.classList.add('font-card--comicsans');
-  } else if (dataStorageRetrieved.typography === '3') {
-    cardPreview.classList.remove('font-card--comicsans', 'font-card--ubuntu', 'font-card--montserrat');
-    cardPreview.classList.add('font-card--montserrat');
-  }
-}
